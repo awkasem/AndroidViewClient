57a5907afef9755f97685b59939f6254349b65bc
awkasem <info@wkasem.com>
2014-12-05 17:25:03 -0500

    Add tabs to Control Panel

--- src/com/dtmilano/android/controlpanel.py
+++ src/com/dtmilano/android/controlpanel.py
@@ -16,107 +16,107 @@
     limitations under the License.
     
     @author: Diego Torres Milano
-    @author: AK
+    @author: Ahmed Kasem
     '''
 
 __version__ = '8.15.3'
 
+import sys, os
 import Tkinter
-import sys
+import tkFileDialog
+from ttk import *
 
+from com.dtmilano.android.viewclient import ViewClient, View
 from com.dtmilano.android.culebron import Operation, Unit, Color
 
 
 class Key:
-    COMMA = 'KEYCODE_COMMA'
-    PERIOD = 'KEYCODE_PERIOD'
-    GO = 'KEYCODE_ENTER'
+    COMMA='KEYCODE_COMMA'
+    PERIOD='KEYCODE_PERIOD'
+    GO='KEYCODE_ENTER'
+
+class ButtonWidth:
+    KEYCODE_BUTTON_WIDTH=25
+    KEYBOARD_BUTTON_WIDTH=1 
 
 class ControlPanel(Tkinter.Toplevel):
 
     def __init__(self, culebron, vc, printOperation, **kwargs):
         self.culebron = culebron
         self.parent = culebron.window
-        Tkinter.Toplevel.__init__(self, self.parent)
-        self.title("Control Panel")
-        self.resizable(0, 0)
-        self.printOperation = printOperation
-        self.vc = vc
-        self.grid()
-        self.column = 0
-        self.row = 0
+        self.child_window = Tkinter.Toplevel(self.parent)
+        self.notebook = Notebook(self.child_window)
+        self.notebook.pack(fill=Tkinter.BOTH, padx=2, pady=3)
+        self.keycode_tab = Frame(self.notebook)
+        self.keyboard_tab = Frame(self.notebook)
+        self.notebook.add(self.keycode_tab, text="KEYCODE")
+        self.notebook.add(self.keyboard_tab, text="KEYBOARD")
+        self.child_window.title("Control Panel")
+        self.child_window.resizable(width=Tkinter.FALSE, height=Tkinter.FALSE)
+        self.child_window.printOperation = printOperation
+        self.child_window.vc = vc
+        self.child_window.grid()
+        self.child_window.column = 0
+        self.child_window.row = 0
+        self.keycode_list = [
+                             'KEYCODE_1', 'KEYCODE_6', 'KEYCODE_BACK', 'KEYCODE_DPAD_UP', 'KEYCODE_PAGE_UP',
+                             'KEYCODE_2', 'KEYCODE_7', 'KEYCODE_SPACE', 'KEYCODE_DPAD_DOWN', 'KEYCODE_PAGE_DOWN',
+                             'KEYCODE_3', 'KEYCODE_8', 'KEYCODE_ENTER', 'KEYCODE_DPAD_LEFT', 'KEYCODE_VOLUME_UP',
+                             'KEYCODE_4', 'KEYCODE_9', 'KEYCODE_DEL', 'KEYCODE_DPAD_RIGHT', 'KEYCODE_VOLUME_DOWN',
+                             'KEYCODE_5', 'KEYCODE_0', 'KEYCODE_SEARCH', 'KEYCODE_DPAD_CENTER', 'KEYCODE_VOLUME_MUTE',
+                             'KEYCODE_TV',  'KEYCODE_POWER', 'KEYCODE_EXPLORER', 'KEYCODE_MENU', 'KEYCODE_CALENDAR',
+                             'KEYCODE_CHANNEL_UP', 'KEYCODE_GUIDE', 'KEYCODE_ZOOM_IN', 'KEYCODE_APP_SWITCH', 'KEYCODE_CALCULATOR',
+                             'KEYCODE_CHANNEL_DOWN', 'KEYCODE_SETTINGS', 'KEYCODE_ZOOM_OUT', 'KEYCODE_HOME', 'KEYCODE_CAMERA',
+                             'KEYCODE_MUSIC', 'KEYCODE_BOOKMARK', 'KEYCODE_CALL', 'KEYCODE_BRIGHTNESS_UP', 'KEYCODE_BRIGHTNESS_DOWN',
+                             'KEYCODE_FORWARD', 'KEYCODE_BUTTON_MODE', 'SNAPSHOPT', 'REFRESH', 'QUIT'
+                            ]
 
-        
-        buttons_list = [
-                   'KEYCODE_1', 'KEYCODE_6', 'KEYCODE_BACK', 'KEYCODE_DPAD_UP', 'KEYCODE_PAGE_UP',
-                   'KEYCODE_2', 'KEYCODE_7', 'KEYCODE_SPACE', 'KEYCODE_DPAD_DOWN', 'KEYCODE_PAGE_DOWN',
-                   'KEYCODE_3', 'KEYCODE_8', 'KEYCODE_ENTER', 'KEYCODE_DPAD_LEFT', 'KEYCODE_VOLUME_UP',
-                   'KEYCODE_4', 'KEYCODE_9', 'KEYCODE_DEL', 'KEYCODE_DPAD_RIGHT', 'KEYCODE_VOLUME_DOWN',
-                   'KEYCODE_5', 'KEYCODE_0', 'KEYCODE_SEARCH', 'KEYCODE_DPAD_CENTER', 'KEYCODE_VOLUME_MUTE',
-                   'KEYCODE_TV',  'KEYCODE_POWER', 'KEYCODE_EXPLORER', 'KEYCODE_MENU', 'KEYCODE_CALENDAR',
-                   'KEYCODE_CHANNEL_UP', 'KEYCODE_GUIDE', 'KEYCODE_ZOOM_IN', 'KEYCODE_APP_SWITCH', 'KEYCODE_CALCULATOR',
-                   'KEYCODE_CHANNEL_DOWN', 'KEYCODE_SETTINGS', 'KEYCODE_ZOOM_OUT', 'KEYCODE_HOME', 'KEYCODE_CAMERA',
-                   'KEYCODE_MUSIC', 'KEYCODE_BOOKMARK', 'KEYCODE_CALL', 'KEYCODE_BRIGHTNESS_UP', 'KEYCODE_BRIGHTNESS_DOWN',
-                   'KEYCODE_FORWARD', 'KEYCODE_BUTTON_MODE', 'SNAPSHOPT', 'REFRESH', 'QUIT'
-                  ]
+        self.keyboard_list = [
+                              'KEYCODE_Q', 'KEYCODE_W', 'KEYCODE_E', 'KEYCODE_R', 'KEYCODE_T', 'KEYCODE_Y', 'KEYCODE_U', 'KEYCODE_I', 'KEYCODE_O', 'KEYCODE_P',
+                              'KEYCODE_A', 'KEYCODE_S', 'KEYCODE_D', 'KEYCODE_F', 'KEYCODE_G', 'KEYCODE_H', 'KEYCODE_J', 'KEYCODE_K', 'KEYCODE_L',
+                              'KEYCODE_Z', 'KEYCODE_X', 'KEYCODE_C', 'KEYCODE_V', 'KEYCODE_B', 'KEYCODE_N', 'KEYCODE_M', 'KEYCODE_DEL',
+                              'KEYCODE_,', 'KEYCODE_.', 'KEYCODE_GO'
+                             ]
 
 
-        for button in buttons_list:
-            self.button = ControlPanelButton(self, culebron, vc, printOperation, value=button, text=button, width=25,
-                                     bg=Color.DARK_GRAY, fg=Color.LIGHT_GRAY, highlightbackground=Color.DARK_GRAY)
+        ### KEYCODE ###
+        for keycode in self.keycode_list:
+            self.keycode = ControlPanelButton(self.keycode_tab, culebron, vc, printOperation, value=keycode, text=keycode,
+                                              width=ButtonWidth.KEYCODE_BUTTON_WIDTH, bg=Color.DARK_GRAY, fg=Color.LIGHT_GRAY,
+                                              highlightbackground=Color.DARK_GRAY)
 
-            if button == 'REFRESH':
-                self.button.configure(fg=Color.BLUE, bg=Color.DARK_GRAY, command=self.button.refreshScreen)
-                self.button.grid(column=self.column, row=self.row)
-            elif button == 'SNAPSHOPT':
-                self.button.configure(fg=Color.BLUE, bg=Color.DARK_GRAY, command=self.button.takeSnapshoot)
-                self.button.grid(column=self.column, row=self.row)
-            elif button == 'QUIT':
-                self.button.configure(fg=Color.BLUE, bg=Color.DARK_GRAY, command=self.destroy)
-                self.button.grid(column=self.column, row=self.row)
+            if keycode == 'REFRESH':
+                self.keycode.configure(fg=Color.BLUE, bg=Color.DARK_GRAY, command=self.keycode.refreshScreen)
+                self.keycode.grid(column=self.child_window.column, row=self.child_window.row)
+            elif keycode == 'SNAPSHOPT':
+                self.keycode.configure(fg=Color.BLUE, bg=Color.DARK_GRAY, command=self.keycode.takeSnapshot)
+                self.keycode.grid(column=self.child_window.column, row=self.child_window.row)
+            elif keycode == 'QUIT':
+                self.keycode.configure(fg=Color.BLUE, bg=Color.DARK_GRAY, command=self.child_window.destroy)
+                self.keycode.grid(column=self.child_window.column, row=self.child_window.row)
             else:
-                self.button.configure(command=self.button.pressKeyCode)
-                self.button.grid(column=self.column, row=self.row)
+                self.keycode.configure(command=self.keycode.pressKeyCode)
+                self.keycode.grid(column=self.child_window.column, row=self.child_window.row)
 
-            self.column += 1
-            if self.column > 4:
-                self.column = 0
-                self.row += 1
+            self.child_window.column += 1
+            if self.child_window.column > 4:
+                self.child_window.column = 0
+                self.child_window.row += 1
 
-class KeyBoard(Tkinter.Toplevel):
-
-    def __init__(self, culebron, vc, printOperation, **kwargs):
-        self.culebron = culebron
-        self.parent = culebron.window
-        Tkinter.Toplevel.__init__(self, self.parent)
-        self.title("Android Keyboard")
-        self.resizable(0, 0)
-        self.transient(self.parent)
-        self.printOperation = printOperation
-        self.vc = vc
-        self.grid()
-        self.column = 0
-        self.row = 0
-
-        
-        buttons_list = ['KEYCODE_Q', 'KEYCODE_W', 'KEYCODE_E', 'KEYCODE_R', 'KEYCODE_T', 'KEYCODE_Y', 'KEYCODE_U', 'KEYCODE_I', 'KEYCODE_O', 'KEYCODE_P',
-                        'KEYCODE_A', 'KEYCODE_S', 'KEYCODE_D', 'KEYCODE_F', 'KEYCODE_G', 'KEYCODE_H', 'KEYCODE_J', 'KEYCODE_K', 'KEYCODE_L',
-                        'KEYCODE_Z', 'KEYCODE_X', 'KEYCODE_C', 'KEYCODE_V', 'KEYCODE_B', 'KEYCODE_N', 'KEYCODE_M', 'KEYCODE_DEL',
-                        'KEYCODE_,', 'KEYCODE_.', 'KEYCODE_GO'
-                        ]
-
-
-        for button in buttons_list:
-            self.button = ControlPanelButton(self, culebron, vc, printOperation, value=button, text=button[8:], width=1,
-                                     bg=Color.DARK_GRAY, fg=Color.LIGHT_GRAY, highlightbackground=Color.DARK_GRAY)
+        ### KEYBOARD ###
+        for keyboard in self.keyboard_list:
+            self.keyboard = ControlPanelButton(self.keyboard_tab, culebron, vc, printOperation, value=keyboard, text=keyboard[8:],
+                                               width=ButtonWidth.KEYBOARD_BUTTON_WIDTH, bg=Color.DARK_GRAY, fg=Color.LIGHT_GRAY,
+                                               highlightbackground=Color.DARK_GRAY)
                 
-            self.button.configure(command=self.button.pressKeyCode)
-            self.button.grid(column=self.column, row=self.row)
+            self.keyboard.configure(command=self.keyboard.pressKeyCode)
+            self.keyboard.grid(column=self.child_window.column, row=self.child_window.row)
 
-            self.column += 1
-            if self.column > 9:
-                self.column = 0
-                self.row += 1
+            self.child_window.column += 1
+            if self.child_window.column > 9:
+                self.child_window.column = 0
+                self.child_window.row += 1
+
 
 class ControlPanelButton(Tkinter.Button):
 
@@ -147,5 +147,8 @@ class ControlPanelButton(Tkinter.Button):
         self.culebron.showVignette()
         self.culebron.takeScreenshotAndShowItOnWindow()
 
-    def takeSnapshoot(self):
-        self.device.takeSnapshot().save('Snapshot', 'PNG')
+    def takeSnapshot(self):
+        #FIXME: Add printOperation <printSaveViewScreenshot(view, foldername)>
+        path = tkFileDialog.asksaveasfilename(parent=self.master, defaultextension=".png", initialfile='Snapshot')
+        if path:
+            self.device.takeSnapshot(reconnect=True).save(path)
